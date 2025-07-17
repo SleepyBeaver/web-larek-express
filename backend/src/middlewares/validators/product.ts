ab80c2b3016c4cb2ffc8086c8938475547b941fc
Kuznetsov Dmitry <dima.mr-smith@yandex.ru>
2025-07-17 17:36:59 +0300

    2.0 Реализация первых 5 шагов проекта Web-ларёк. Тёмная сторона

---
+++ backend/src/middlewares/validators/product.ts
@@ -0,0 +1,35 @@
+import { Joi, Segments } from 'celebrate';
+
+export const validateCreateProduct = {
+  [Segments.BODY]: Joi.object().keys({
+    title: Joi.string().min(2).max(30).required().label('Название').messages({
+      'string.base': '{#label} должно быть строкой',
+      'string.min': '{#label} должно содержать минимум {#limit} символа',
+      'string.max': '{#label} должно содержать максимум {#limit} символов',
+      'any.required': '{#label} обязательно для заполнения',
+    }),
+    category: Joi.string().required().label('Категория').messages({
+      'string.base': '{#label} должна быть строкой',
+      'any.required': '{#label} обязательна для заполнения',
+    }),
+    description: Joi.string().required().label('Описание').messages({
+      'string.base': '{#label} должно быть строкой',
+      'any.required': '{#label} обязательно для заполнения',
+    }),
+    price: Joi.number().allow(null).label('Цена').messages({
+      'number.base': '{#label} должна быть числом или null',
+    }),
+    image: Joi.object({
+      fileName: Joi.string().required().label('Имя файла').messages({
+        'string.base': '{#label} должно быть строкой',
+        'any.required': '{#label} обязательно для заполнения',
+      }),
+      originalName: Joi.string().required().label('Оригинальное имя файла').messages({
+        'string.base': '{#label} должно быть строкой',
+        'any.required': '{#label} обязательно для заполнения',
+      }),
+    }).required().label('Изображение').messages({
+      'any.required': '{#label} обязательно для заполнения',
+    }),
+  }),
+};
